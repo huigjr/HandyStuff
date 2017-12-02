2213b05cb3bca4dc87e77503bea81cb90db7013a
huigjr <huigjr@gmail.com>
2017-12-02 14:36:17 +0100

    update diagnostics

--- diagnostics.php
+++ diagnostics.php
@@ -18,7 +18,7 @@ foreach($_ENV as $key => $value) $vars[] = array('ENV',$key,$value);
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
   <style>
-*{margin:0;padding:0;border:0;box-sizing:border-box}html{font-family:Arial,sans-serif;font-size:.8em;background:#efefef}.contentblock{width:100%;max-width:1200px;margin:10px auto;background:#fff;padding:10px}.extention,tr:nth-child(odd){background:#eee}table{width:100%}tr{text-align:left}caption,td,th{padding:5px}.extention{display:inline-block;width:116px;padding:4px;margin:4px;text-align:center}
+  .extention,footer{text-align:center}*{margin:0;padding:0;border:0;box-sizing:border-box}html{font-family:Arial,sans-serif;font-size:.8em;background:#efefef}.contentblock{width:100%;max-width:1200px;margin:10px auto;background:#fff;padding:10px}.extention,tr:nth-child(odd){background:#eee}table{width:100%}tr{text-align:left}caption,td,th{padding:5px}.extention{display:inline-block;width:116px;padding:4px;margin:4px}footer{background:#aaa;font-size:10px;line-height:50px}
   </style>
 </head>
 <body>
@@ -65,5 +65,82 @@ foreach($_ENV as $key => $value) $vars[] = array('ENV',$key,$value);
   </table>
 </div>
 
+<div class="contentblock">
+	<table>
+    <caption><h2>JS</h2></caption>
+    <tr>
+      <th>Key</th>
+      <th>Value</th>
+    </tr>
+    <tr>
+      <td>navigator.oscpu</td>
+      <td><script>document.write(navigator.oscpu)</script></td>
+    </tr>
+    <tr>
+      <td>navigator.hardwareConcurrency</td>
+      <td><script>document.write(navigator.hardwareConcurrency)</script></td>
+    </tr>
+    <tr>
+      <td>navigator.doNotTrack</td>
+      <td><script>document.write(navigator.doNotTrack)</script></td>
+    </tr>
+    <tr>
+      <td>navigator.permissions</td>
+      <td><script>document.write(navigator.permissions)</script></td>
+    </tr>
+    <tr>
+      <td>navigator.maxTouchPoints</td>
+      <td><script>document.write(navigator.maxTouchPoints)</script></td>
+    </tr>
+    <tr>
+      <td>navigator.platform</td>
+      <td><script>document.write(navigator.platform)</script></td>
+    </tr>
+  </table>
+</div>
+
+<div class="contentblock">
+  <h2>Javascript Navigator Object</h2>
+  <p id="navigator"></p>
+</div>
+
+<div class="contentblock">
+  <h2>Javascript Window Object</h2>
+  <p id="window"></p>
+</div>
+
+<div class="contentblock">
+  <h2>Javascript Postion Object</h2>
+  <p id="position"></p>
+</div>
+
+<div class="contentblock" id="test">
+  <h2>Javascript Test Object</h2>
+</div>
+
+<footer>Environment Diagnostics - Huig - 2017</footer>
+
+<script>
+function objToJson(myObj){
+  var output = {};
+  for (var i in myObj) output[i] = myObj[i];
+  return JSON.stringify(output);
+}
+var jsonnav = objToJson(navigator);
+document.getElementById("navigator").innerHTML = jsonnav;
+document.getElementById("window").innerHTML = objToJson(window.screen);
+document.getElementById("position").innerHTML = position.toSource();
+</script>
+<script>
+function objToString (obj) {
+    var str = '';
+    for (var p in obj) {
+        if (obj.hasOwnProperty(p)) {
+            str += p + '::' + obj[p] + '\n';
+        }
+    }
+    return str;
+}
+</script>
 </body>
 </html>
